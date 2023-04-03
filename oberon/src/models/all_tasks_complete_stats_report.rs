b246da0e9c7f0dea3b49b648ffc3ee0ed092d794
Jesson Yo <Aphostrophy@gmail.com>
2023-04-03 12:29:43 +0700

    [oberon] reporter for all_tasks_complete_stats_report

--- oberon/src/models/all_tasks_complete_stats_report.rs
+++ oberon/src/models/all_tasks_complete_stats_report.rs
@@ -3,6 +3,8 @@ use std::io::{BufWriter, Write};
 
 use serde::{Deserialize, Serialize};
 
+use crate::sched_math::{duration_ns_to_fmt_duration, prio_to_nice};
+
 use super::task_statistics::TaskStatistics;
 use super::tasks_sched_stats_report::TasksSchedStatsReport;
 
@@ -22,11 +24,111 @@ impl TasksSchedStatsReport for AllTasksCompleteStatsReport {
         let file = File::create(filename)?;
         let mut writer = BufWriter::new(file);
 
-        // TODO: Implement
+        self.report_aggregate_sched_stats(&mut writer)?;
 
-        writer.write(b"PID\t PRIO\n")?;
+        writer.write_fmt(format_args!("\n"))?;
+
+        self.report_sched_stats(&mut writer)?;
+
+        writer.write_fmt(format_args!("\n"))?;
+
+        self.report_sched_stats_analysis(&mut writer)?;
 
         writer.flush()?;
+
+        Ok(())
+    }
+}
+
+impl AllTasksCompleteStatsReport {
+    fn report_aggregate_sched_stats(&self, writer: &mut BufWriter<File>) -> std::io::Result<()> {
+        writer.write_fmt(format_args!("Tasks:\t {} total\n", self.num_tasks))?;
+        Ok(())
+    }
+
+    fn report_sched_stats(&self, writer: &mut BufWriter<File>) -> std::io::Result<()> {
+        writer.write_fmt(format_args!(
+            "{} STATISTICS {}\n",
+            "=".repeat(5),
+            "=".repeat(5)
+        ))?;
+
+        writer.write_fmt(format_args!(
+            "{}PID{}PRIO{}NICE{}TIME+{}COMMAND{}TOTAL_CPU(NS){}TOTAL_WAIT(NS){}NR_SWITCH\n",
+            " ".repeat(4),
+            " ".repeat(1),
+            " ".repeat(2),
+            " ".repeat(8),
+            " ".repeat(11),
+            " ".repeat(5),
+            " ".repeat(4),
+            " ".repeat(3),
+        ))?;
+
+        let n = self.tasks_stats.len();
+
+        for i in 0..n {
+            let t = &self.tasks_stats[i];
+
+            writer.write_fmt(format_args!(
+                "{:>7} {:>4} {:>5} {:>8} {:>18} {:>17} {:>17} {:>11}\n",
+                t.pid,
+                t.prio,
+                prio_to_nice(t.prio),
+                duration_ns_to_fmt_duration(t.last_ktime_ns - t.sched_stats_start_time_ns),
+                t.comm,
+                t.total_cpu_time_ns,
+                t.total_wait_time_ns,
+                t.nr_switches,
+            ))?;
+        }
+
+        Ok(())
+    }
+
+    fn report_sched_stats_analysis(&self, writer: &mut BufWriter<File>) -> std::io::Result<()> {
+        writer.write_fmt(format_args!(
+            "{} ANALYSIS {}\n",
+            "=".repeat(5),
+            "=".repeat(5)
+        ))?;
+        writer.write_fmt(format_args!("\n"))?;
+
+        writer.write_fmt(format_args!(
+            "{}PID{}FAIR_NS{}IDEAL_FAIR_NS{}PRIO{}NICE{}RECO_PRIO{}RECO_NICE\n",
+            " ".repeat(4),
+            " ".repeat(4),
+            " ".repeat(4),
+            " ".repeat(4),
+            " ".repeat(4),
+            " ".repeat(4),
+            " ".repeat(4),
+        ))?;
+
+        let n = self.tasks_stats.len();
+
+        for i in 0..n {
+            let t = &self.tasks_stats[i];
+            let actual_share = &self.tasks_normalized_cpu_fair_share_ns[i];
+            let ideal_share = &self.tasks_ideal_normalized_cpu_fair_share_ns[i];
+
+            writer.write_fmt(format_args!(
+                "{:>7} {:>10} {:>16} {:>7} {:>7} {:>12} {:>12}\n",
+                t.pid,
+                format!("{:.2}", actual_share),
+                ideal_share,
+                t.prio,
+                prio_to_nice(t.prio),
+                "-",
+                "-"
+            ))?;
+        }
+
+        writer.write_fmt(format_args!("\n"))?;
+        writer.write_fmt(format_args!(
+            "### NOTE : FAIR SHARES ARE NORMALIZED TO SCHED_LATENCY FOR ANALYSIS\n"
+        ))?;
+        writer.write_fmt(format_args!("\n"))?;
 
         Ok(())
     }
