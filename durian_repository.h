431de1a33a8e1a4c214bcae188900d47fde85764
Jesson Yo <Aphostrophy@gmail.com>
2023-05-01 02:29:15 +0700

    [oberond] docs: function doc for repository_track_task, repository_untrack_task

--- durian_repository.h
+++ durian_repository.h
@@ -18,7 +18,7 @@
  * @param task_state The most recent visible state of the task
  * @param ktime_ns The kernel time in nanoseconds in when the sched event happens
  *
- * Simply adds an entry for tracking task statistics.
+ * Simply adds an entry for tracking task statistics and insert its pid to the RUNNING_PID_SET.
  * The repository will track the task's total CPU time, total wait time, last seen task state,
  * and kernel time which indicates the time the task changes state.
  * It will be set to its initial values.
@@ -36,10 +36,15 @@ void repository_track_task(durian_ctx *ctx, int pid, const char comm[16], int pr
  *
  * @param ctx The durian app context, which contains a reference to the Redis connection.
  * @param pid The process ID of the task to be tracked.
- * @param task_state The most recent visible state of the task
- * @param ktime_ns The kernel time in nanoseconds in when the sched event happens
+ * @param task_state The most recent visible state of the task.
+ * @param ktime_ns The kernel time in nanoseconds in when the sched event happens.
  *
- * Simply changes the last_seen_state of a task to __TASK_STOPPED
+ * Changes the last_seen_state of a task to __TASK_STOPPED.
+ * Removes the pid of the task from the RUNNING_PID_SET.
+ * Set an expiry for all entries related to the pid to be removed from the repository.
+ * An expiry is used instead of a direct removal to prevent the client from breaking if it
+ * has already fetched the RUNNING_PID_SET that still contains the pid.
+ *
  * @note This function does not execute the Redis command. To execute the pipeline, call `redisGetReply` repeatedly after pushing all desired commands.
  */
 void repository_untrack_task(durian_ctx *ctx, int pid, unsigned long long ktime_ns);
